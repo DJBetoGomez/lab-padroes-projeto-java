0d260d328222927c6bc57f3c965219d3252ccb35
robertoferreira <rgdferreira@hotmail.com>
2022-03-07 19:06:52 -0300

    First commit

---
+++ src/one/digital/innovation/gof/SingletonEager.java
@@ -0,0 +1,13 @@
+package one.digital.innovation.gof;
+
+public class SingletonEager {
+
+    public static SingletonEager instancia  = new SingletonEager();
+
+    private SingletonEager(){
+        super();
+    }
+    public static SingletonEager getInstancia() {
+        return instancia;
+    }
+}
